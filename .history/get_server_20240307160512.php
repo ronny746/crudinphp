54d33a8161b827cd6f8d63fe4f0aed454966f5c6
Rohit Rana <rkrana6631@gmail.com>
2024-03-07 18:19:38 +0530

    ne

--- .history/get_server_20240307160512.php
+++
@@ -1,4 +0,0 @@
-<?php
-$currentDateTime = date('Y-m-d H:i:s'); // Format: YYYY-MM-DD HH:MM:SS
-echo $currentDateTime;
-?>
--- .history/get_server_20240307160609.php
+++
@@ -1,4 +0,0 @@
-<?php
-$currentDateTime = date('Y-m-d H:i:s'); // Current date and time in server's time zone
-echo $currentDateTime;
-?>
